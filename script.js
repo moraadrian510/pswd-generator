3cfa64f71d42962fa9d586bed9423e0ca17be17b
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2022-12-21 14:11:08 -0800

    password genarator com

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+// crear array de cada tipo de los caracteres.
+
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+// empty array
+ 
+var resultArray = [];
+var userArray = [];
+
+uppercaseArray [1]
+//-----------------------------------------------------------------
+  var numCharacter = prompt ("How much number of Character you want between 8 and 128?");
+  var numbers = confirm ("you want numbers in your password?");
+  var uppercases = confirm ("you want Uppercases in your password?");
+  var lowercases = confirm ("you want lowercases in your password?");
+  var characters = confirm ("you want special characters in your password?");
+
+
+// condition of the array
+
+if (numbers){
+  resultArray = resultArray.concat(numbArray);
+  
+}
+
+if (uppercases){
+  resultArray = resultArray.concat(uppercaseArray);
+
+}
+
+if (lowercases){
+  resultArray = resultArray.concat(lowercaseArray);
+
+}
+
+if (characters){
+  resultArray = resultArray.concat(characterArray);
+}
+console.log(resultArray)
+
+
+for (var i = 0; i < numCharacter; i++) {
+      
+  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
+  }
+
+  return userArray.join("") ;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,7 +62,15 @@ function writePassword() {
 
   passwordText.value = password;
 
+  // copyBtn.removeAttribute("disabled");
+  // copyBtn.focus();
+}
+
+function copyToClipboard() {
+  // BONUS 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// BONUS EVENT LISTENER
