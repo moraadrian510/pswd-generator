ccffb8c4ce701a95a64326f5a3fdbd635a6ebc1b
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2022-12-21 14:22:55 -0800

    css style

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-// crear array de cada tipo de los caracteres.
+
 
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
-// empty array
+
  
 var resultArray = [];
 var userArray = [];
@@ -67,10 +67,7 @@ function writePassword() {
 }
 
 function copyToClipboard() {
-  // BONUS 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// BONUS EVENT LISTENER
